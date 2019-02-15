3935f7dc25f19062b81b8964b7100fe20d9264f1
1809591 <1809591@rgu.ac.uk>
2019-02-15 13:06:42 +0000

    2nd day

--- PHP_Lab/index.php
+++ PHP_Lab/index.php
@@ -2,11 +2,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="age_input.html">
 </head>
 <body>
 
 <p>
     <?php
+    $myage = $_GET["age"];
+
+    if($myage<16){
+         echo "You cannot purchase any products";
+    }
+    else if($myage>=16 and $myage<18){
+        echo "You may purchase specs but not mugs or sausage rolls";
+    }
+    else if($myage>=18 and $myage<21){
+        echo "You may purchase specs and mugs but not sausage rolls";
+    }
+    else{
+        echo "You may purchase any products";
+    }
 
     ?>
 </p>
